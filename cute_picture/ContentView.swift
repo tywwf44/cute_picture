6c716423374ce8a65c2ca378f8560b414fe3a61b
tywwf44 <tywwf44@gmail.com>
2020-11-19 15:02:38 +0800

    Initial Commit

---
+++ cute_picture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cute_picture
+//
+//  Created by   lawliet on 2020/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
